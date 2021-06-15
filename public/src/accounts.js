8bbd69e3140cccc418ab62ba74c6eca269d5852f
Linh <lcahu91@gmail.com>
2021-06-15 15:47:48 -0400

    reformating

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,27 +1,29 @@
-const books = require('../../test/fixtures/books.fixture');
-const accounts = require('../../test/fixtures/accounts.fixture');
-const authors = require('../../test/fixtures/authors.fixture');
-
+const books = require("../../test/fixtures/books.fixture");
+const accounts = require("../../test/fixtures/accounts.fixture");
+const authors = require("../../test/fixtures/authors.fixture");
 
 function findAccountById(accounts, id) {
   return accounts.find((account) => account.id === id);
 }
 
 function sortAccountsByLastName(accounts) {
-  return accounts.sort((a,b) => {
+  return accounts.sort((a, b) => {
     var nameA = a.name.last.toUpperCase();
     var nameB = b.name.last.toUpperCase();
-    return nameA > nameB ? 1:-1;
-  })
+    return nameA > nameB ? 1 : -1;
+  });
 }
 
 function getTotalNumberOfBorrows(account, books) {
   // Array of all borrow transactions
-  let borrowTransactions = books.reduce((acc, book) => [...acc, ...book.borrows], []);
+  let borrowTransactions = books.reduce(
+    (acc, book) => [...acc, ...book.borrows],
+    []
+  );
   // Extract the Account IDs from all burrow transactions
-  let burrowingAccounts = borrowTransactions.map(borrow => borrow.id);
+  let burrowingAccounts = borrowTransactions.map((borrow) => borrow.id);
   // Filter to account IDs of focus and count transactions
-  return burrowingAccounts.filter((id) => id === account.id).length; 
+  return burrowingAccounts.filter((id) => id === account.id).length;
 }
 
 // Prioritize for refactoring
@@ -29,16 +31,21 @@ function getBooksPossessedByAccount(account, books, authors) {
   let booksPossessedByAccount = [];
 
   for (let book in books) {
-      let borrowTransactions = books[book].borrows;
-      for (let transaction in borrowTransactions) {
-          let borrowTransaction = borrowTransactions[transaction];
-          // Check if account has borrowed book and has not returned it (currently possessed)
-          if (borrowTransaction.id === account.id && borrowTransaction.returned === false) {
-              // embed author object inside of book object
-              books[book].author = authors.find(author => author.id === books[book].authorId);
-              booksPossessedByAccount.push(books[book]);
-          }
+    let borrowTransactions = books[book].borrows;
+    for (let transaction in borrowTransactions) {
+      let borrowTransaction = borrowTransactions[transaction];
+      // Check if account has borrowed book and has not returned it (currently possessed)
+      if (
+        borrowTransaction.id === account.id &&
+        borrowTransaction.returned === false
+      ) {
+        // embed author object inside of book object
+        books[book].author = authors.find(
+          (author) => author.id === books[book].authorId
+        );
+        booksPossessedByAccount.push(books[book]);
       }
+    }
   }
   return booksPossessedByAccount;
 }
